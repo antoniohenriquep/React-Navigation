1107a15327afdbfc3508f4e0d0759577d5f3c4be
Antonio Pedreira <antoniohenriquep@outlook.com>
2023-01-18 16:49:27 -0300

    Adicionando tab navigator

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/Pages/Home';
 import About from './src/Pages/About';
 
 const Stack = createNativeStackNavigator()
+
+/* 
+  Os componentes de tipos de navegação devem estar dentro do NavigationContainer. Seguindo a seguinte estrutura: NavigationContainer > Xnavigator.Navigator > Xnavigator.Screen
+  Tipos de navegação:
+  - Stack navigator: Como o nome já diz, navegação em pilha. Geralmente a função de navegar é chamada por um botão na página
+  - Tab navigator: Rodapé que contém os botões de navegação. Como o Instagram, por exemplo.
+  - Drawer: Navegação em gaveta, que sai das laterais da página
+*/
 
 export default function App() {
  return (
